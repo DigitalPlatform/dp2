6948caa1b00a47c9c2a4c6dd347309d2db9383ae
XieTao <xietao@dp2003.com>
2015-05-09 23:22:47 +0800

    为 dp2libraryXE 添加 copydata.bat

--- dp2LibraryXE/MainForm.cs
+++ dp2LibraryXE/MainForm.cs
@@ -2262,9 +2262,9 @@ http://dp2003.com" + (this.IsServer == false ? "" : @"
                                     WriteLibraryEventLog(strText, EventLogEntryType.Information);
 #endif
                                      * */
+                                    // 时间有可能不一致，可能是夏令时之类的问题
                                     File.SetLastWriteTime(strFullPath, e.LastModified);
                                 }
-                                // 时间有可能不一致，可能是夏令时之类的问题
                                 Debug.Assert(e.LastModified == File.GetLastWriteTime(strFullPath));
                                 _versionManager.SetFileVersion(strFullPath, e.LastModified.ToString());
                             }
---
+++ dp2LibraryXE/copydata.bat
@@ -0,0 +1,3 @@
+
+..\ziputil kernel_data kernel_data.zip -t
+..\ziputil library_data library_data.zip -t
