1c0f778851f8591b7460aba8268233bc7f079b69
XieTao <xietao@dp2003.com>
2015-05-26 10:41:59 +0800

    添加两个 bat 文件

---
+++ dp2LibraryXE/copyinterop.bat
@@ -0,0 +1,11 @@
+echo copy sqlite.interop.dll to x86 and x64 from nuget packages directory
+
+MD x86
+MD x64
+
+CD ..\packages\system.data.sqlite.core.*
+
+copy .\build\net40\x86\sqlite.interop.dll ..\..\dp2circulation\x86
+copy .\build\net40\x64\sqlite.interop.dll ..\..\dp2circulation\x64
+
+CD ..\..\dp2circulation
