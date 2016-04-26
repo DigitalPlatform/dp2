4ff1ffb62c59d47dc3003cacce02c19d3cd3bc52
DigitalPlatform <ll_rr_dd@hotmail.com>
2016-04-26 22:08:30 +0800

    dp2circulation 尝试兼容高 DPI

---
+++ dp2LibraryXE/opac_app/Statis.aspx.designer.cs
@@ -0,0 +1,148 @@
+﻿//------------------------------------------------------------------------------
+// <自动生成>
+//     此代码由工具生成。
+//
+//     对此文件的更改可能会导致不正确的行为，并且如果
+//     重新生成代码，这些更改将会丢失。 
+// </自动生成>
+//------------------------------------------------------------------------------
+
+
+
+public partial class Statis {
+    
+    /// <summary>
+    /// Linkcontrol1 控件。
+    /// </summary>
+    /// <remarks>
+    /// 自动生成的字段。
+    /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+    /// </remarks>
+    protected global::DigitalPlatform.OPAC.Web.LinkControl Linkcontrol1;
+    
+    /// <summary>
+    /// Linkcontrol2 控件。
+    /// </summary>
+    /// <remarks>
+    /// 自动生成的字段。
+    /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+    /// </remarks>
+    protected global::DigitalPlatform.OPAC.Web.LinkControl Linkcontrol2;
+    
+    /// <summary>
+    /// form1 控件。
+    /// </summary>
+    /// <remarks>
+    /// 自动生成的字段。
+    /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+    /// </remarks>
+    protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+    
+    /// <summary>
+    /// TitleBarControl1 控件。
+    /// </summary>
+    /// <remarks>
+    /// 自动生成的字段。
+    /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+    /// </remarks>
+    protected global::DigitalPlatform.OPAC.Web.TitleBarControl TitleBarControl1;
+    
+    /// <summary>
+    /// SideBarControl1 控件。
+    /// </summary>
+    /// <remarks>
+    /// 自动生成的字段。
+    /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+    /// </remarks>
+    protected global::DigitalPlatform.OPAC.Web.SideBarControl SideBarControl1;
+    
+    /// <summary>
+    /// HyperLink1 控件。
+    /// </summary>
+    /// <remarks>
+    /// 自动生成的字段。
+    /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+    /// </remarks>
+    protected global::System.Web.UI.WebControls.HyperLink HyperLink1;
+    
+    /// <summary>
+    /// HyperLink2 控件。
+    /// </summary>
+    /// <remarks>
+    /// 自动生成的字段。
+    /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+    /// </remarks>
+    protected global::System.Web.UI.WebControls.HyperLink HyperLink2;
+    
+    /// <summary>
+    /// TextBox_dateRange 控件。
+    /// </summary>
+    /// <remarks>
+    /// 自动生成的字段。
+    /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+    /// </remarks>
+    protected global::System.Web.UI.WebControls.TextBox TextBox_dateRange;
+    
+    /// <summary>
+    /// Button_beginStatis 控件。
+    /// </summary>
+    /// <remarks>
+    /// 自动生成的字段。
+    /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+    /// </remarks>
+    protected global::System.Web.UI.WebControls.Button Button_beginStatis;
+    
+    /// <summary>
+    /// Literal1 控件。
+    /// </summary>
+    /// <remarks>
+    /// 自动生成的字段。
+    /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+    /// </remarks>
+    protected global::System.Web.UI.WebControls.Literal Literal1;
+    
+    /// <summary>
+    /// Calendar1 控件。
+    /// </summary>
+    /// <remarks>
+    /// 自动生成的字段。
+    /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+    /// </remarks>
+    protected global::System.Web.UI.WebControls.Calendar Calendar1;
+    
+    /// <summary>
+    /// PanelControl2 控件。
+    /// </summary>
+    /// <remarks>
+    /// 自动生成的字段。
+    /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+    /// </remarks>
+    protected global::DigitalPlatform.OPAC.Web.PanelControl PanelControl2;
+    
+    /// <summary>
+    /// StatisViewControl1 控件。
+    /// </summary>
+    /// <remarks>
+    /// 自动生成的字段。
+    /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+    /// </remarks>
+    protected global::DigitalPlatform.OPAC.Web.StatisViewControl StatisViewControl1;
+    
+    /// <summary>
+    /// FooterBarControl1 控件。
+    /// </summary>
+    /// <remarks>
+    /// 自动生成的字段。
+    /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+    /// </remarks>
+    protected global::DigitalPlatform.OPAC.Web.FooterBarControl FooterBarControl1;
+    
+    /// <summary>
+    /// HiddenField_activetab 控件。
+    /// </summary>
+    /// <remarks>
+    /// 自动生成的字段。
+    /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+    /// </remarks>
+    protected global::System.Web.UI.WebControls.HiddenField HiddenField_activetab;
+}
