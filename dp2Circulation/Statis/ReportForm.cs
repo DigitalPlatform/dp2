6a634a422723943b1293a2a3b8977dfae57c2d14
XieTao <xietao@dp2003.com>
2016-05-18 00:36:40 +0800

    完善报表窗。报表最末日期配置失当，会在按钮文字上警告。首次创建是不怕中途删除读者记录了

--- dp2Circulation/Statis/ReportForm.cs
+++ dp2Circulation/Statis/ReportForm.cs
@@ -778,7 +778,6 @@ System.Exception: 浏览事项异常: (lStart=293600 index=143)  path=图书总�
                         return -1;
                     }
 
-
                     lRet = this.Channel.GetSearchResult(
                         stop,
                         null,   // strResultSetName
@@ -804,6 +803,10 @@ System.Exception: 浏览事项异常: (lStart=293600 index=143)  path=图书总�
                     for (int i = 0; i < searchresults.Length; i++)
                     {
                         DigitalPlatform.LibraryClient.localhost.Record searchresult = searchresults[i];
+                        if (searchresult.Cols.Length < 5)
+                        {
+                            continue;   // 中途遇到服务器有人删除读者记录，很常见的现象
+                        }
 
                         ReaderLine line = new ReaderLine();
                         line.ReaderRecPath = searchresult.Path;
@@ -1352,7 +1355,6 @@ System.Exception: 浏览事项异常: (lStart=293600 index=143)  path=图书总�
                         if (searchresult.Keys != null && searchresult.Keys.Length > 0)
                             line.Class = searchresult.Keys[0].Key;
                         lines.Add(line);
-
                     }
 
 #if NO
@@ -10699,6 +10701,8 @@ MessageBoxDefaultButton.Button1);
                 // strError = "上次统计最末日期 '"+strLastDay+"' 不应晚于 日志同步最后日期 " + strDailyEndDate + " 的前一天";
                 // return -1;
                 strError = "报表已经是最新";
+                if (start > daily_end)
+                    strError += " (警告!" + strLastDay + "有误)";
                 return 0;
             }
 
