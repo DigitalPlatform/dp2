c6c3ddbf23b1b15d8209c147093c52f6a980bb1f
XieTao <xietao@dp2003.com>
2017-07-06 14:56:58 +0800

    修正零星验收过程中 TargetRecPath 残留的 Bug

--- dp2Circulation/EntityForm/EntityForm.cs
+++ dp2Circulation/EntityForm/EntityForm.cs
@@ -4247,6 +4247,8 @@ true);
             out string strError)
         {
             LibraryChannel channel = this.GetChannel();
+            TimeSpan old_timeout = channel.Timeout;
+            channel.Timeout = new TimeSpan(0, 0, 10);
 
 #if NO
             Progress.OnStop += new StopEventHandler(this.DoStop);
@@ -4254,10 +4256,10 @@ true);
             Progress.BeginLoop();
 #endif
 
+            string strOldMessage = Progress.Message;
+            Progress.SetMessage("正在装入书目记录 " + strBiblioRecPath + " 的局部 ...");
             try
             {
-                Progress.SetMessage("正在装入书目记录 " + strBiblioRecPath + " 的局部 ...");
-                channel.Timeout = new TimeSpan(0, 0, 10);
                 long lRet = channel.GetBiblioInfo(
                     null,   // Progress.State == 0 ? Progress : null,
                     strBiblioRecPath,
@@ -4274,6 +4276,9 @@ true);
                 Progress.OnStop -= new StopEventHandler(this.DoStop);
                 Progress.Initial("");
 #endif
+                Progress.SetMessage(strOldMessage);
+
+                channel.Timeout = old_timeout;
                 this.ReturnChannel(channel);
             }
         }
@@ -8318,7 +8323,7 @@ MessageBoxDefaultButton.Button2);
             {
                 string strAction = "change";
 
-                if (Global.IsAppendRecPath(strPath) == true 
+                if (Global.IsAppendRecPath(strPath) == true
                      || bResave == true
                     )
                     strAction = "new";
