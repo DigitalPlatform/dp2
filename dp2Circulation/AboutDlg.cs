c838f3bd57f916d752f37f5be62b613ed584506c
xietao <xietao@dp2003.com>
2018-06-24 00:51:30 +0800

    整理发布参数，版本号

--- dp2Circulation/AboutDlg.cs
+++ dp2Circulation/AboutDlg.cs
@@ -79,7 +79,7 @@ namespace dp2Circulation
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(461, 17);
             this.label1.TabIndex = 0;
-            this.label1.Text = "dp2 内务/流通 dp2Circulation V2.18";
+            this.label1.Text = "dp2 内务/流通 dp2Circulation V3";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // label_copyright
