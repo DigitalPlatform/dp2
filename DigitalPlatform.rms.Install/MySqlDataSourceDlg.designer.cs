38afdd3d6aeb268229cb964a00081300c9359ca7
xietao <xietao@dp2003.com>
2018-09-23 01:25:01 +0800

    为 dp2kernel (MySQL)增加 SSL mode

--- DigitalPlatform.rms.Install/MySqlDataSourceDlg.designer.cs
+++ DigitalPlatform.rms.Install/MySqlDataSourceDlg.designer.cs
@@ -43,6 +43,7 @@
             this.button_getSqlServerName = new System.Windows.Forms.Button();
             this.textBox_sqlServerName = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.checkBox_ssl = new System.Windows.Forms.CheckBox();
             this.groupBox_login.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -50,21 +51,19 @@
             // 
             this.textBox_confirmLoginPassword.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.textBox_confirmLoginPassword.Location = new System.Drawing.Point(111, 88);
-            this.textBox_confirmLoginPassword.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox_confirmLoginPassword.Location = new System.Drawing.Point(166, 132);
             this.textBox_confirmLoginPassword.Name = "textBox_confirmLoginPassword";
             this.textBox_confirmLoginPassword.PasswordChar = '*';
-            this.textBox_confirmLoginPassword.Size = new System.Drawing.Size(163, 21);
+            this.textBox_confirmLoginPassword.Size = new System.Drawing.Size(242, 28);
             this.textBox_confirmLoginPassword.TabIndex = 5;
             // 
             // label3
             // 
             this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(16, 90);
-            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Location = new System.Drawing.Point(24, 135);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(83, 12);
+            this.label3.Size = new System.Drawing.Size(125, 18);
             this.label3.TabIndex = 4;
             this.label3.Text = "再次输入密码:";
             // 
@@ -72,21 +71,19 @@
             // 
             this.textBox_loginPassword.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.textBox_loginPassword.Location = new System.Drawing.Point(111, 63);
-            this.textBox_loginPassword.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox_loginPassword.Location = new System.Drawing.Point(166, 94);
             this.textBox_loginPassword.Name = "textBox_loginPassword";
             this.textBox_loginPassword.PasswordChar = '*';
-            this.textBox_loginPassword.Size = new System.Drawing.Size(163, 21);
+            this.textBox_loginPassword.Size = new System.Drawing.Size(242, 28);
             this.textBox_loginPassword.TabIndex = 3;
             // 
             // label2
             // 
             this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(16, 66);
-            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Location = new System.Drawing.Point(24, 99);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(53, 12);
+            this.label2.Size = new System.Drawing.Size(80, 18);
             this.label2.TabIndex = 2;
             this.label2.Text = "密码(&P):";
             // 
@@ -100,11 +97,9 @@
             this.groupBox_login.Controls.Add(this.label2);
             this.groupBox_login.Controls.Add(this.textBox_loginName);
             this.groupBox_login.Controls.Add(this.label5);
-            this.groupBox_login.Location = new System.Drawing.Point(12, 173);
-            this.groupBox_login.Margin = new System.Windows.Forms.Padding(2);
+            this.groupBox_login.Location = new System.Drawing.Point(18, 260);
             this.groupBox_login.Name = "groupBox_login";
-            this.groupBox_login.Padding = new System.Windows.Forms.Padding(2);
-            this.groupBox_login.Size = new System.Drawing.Size(411, 129);
+            this.groupBox_login.Size = new System.Drawing.Size(616, 194);
             this.groupBox_login.TabIndex = 13;
             this.groupBox_login.TabStop = false;
             this.groupBox_login.Text = " 指定一个已有的 MySQL 帐户";
@@ -113,10 +108,9 @@
             // 
             this.textBox_loginName.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.textBox_loginName.Location = new System.Drawing.Point(111, 26);
-            this.textBox_loginName.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox_loginName.Location = new System.Drawing.Point(166, 39);
             this.textBox_loginName.Name = "textBox_loginName";
-            this.textBox_loginName.Size = new System.Drawing.Size(163, 21);
+            this.textBox_loginName.Size = new System.Drawing.Size(242, 28);
             this.textBox_loginName.TabIndex = 1;
             this.textBox_loginName.Text = "dp2kernel";
             // 
@@ -124,10 +118,9 @@
             // 
             this.label5.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(16, 29);
-            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Location = new System.Drawing.Point(24, 44);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(65, 12);
+            this.label5.Size = new System.Drawing.Size(98, 18);
             this.label5.TabIndex = 0;
             this.label5.Text = "用户名(&N):";
             // 
@@ -139,22 +132,22 @@
             this.textBox_message.BackColor = System.Drawing.SystemColors.Info;
             this.textBox_message.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.textBox_message.ForeColor = System.Drawing.SystemColors.InfoText;
-            this.textBox_message.Location = new System.Drawing.Point(10, 10);
-            this.textBox_message.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox_message.Location = new System.Drawing.Point(15, 15);
             this.textBox_message.Multiline = true;
             this.textBox_message.Name = "textBox_message";
             this.textBox_message.ReadOnly = true;
             this.textBox_message.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.textBox_message.Size = new System.Drawing.Size(414, 80);
+            this.textBox_message.Size = new System.Drawing.Size(620, 119);
             this.textBox_message.TabIndex = 9;
             // 
             // button_Cancel
             // 
             this.button_Cancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.button_Cancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.button_Cancel.Location = new System.Drawing.Point(347, 351);
+            this.button_Cancel.Location = new System.Drawing.Point(520, 526);
+            this.button_Cancel.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button_Cancel.Name = "button_Cancel";
-            this.button_Cancel.Size = new System.Drawing.Size(75, 23);
+            this.button_Cancel.Size = new System.Drawing.Size(112, 34);
             this.button_Cancel.TabIndex = 17;
             this.button_Cancel.Text = "取消";
             this.button_Cancel.UseVisualStyleBackColor = true;
@@ -163,9 +156,10 @@
             // button_OK
             // 
             this.button_OK.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.button_OK.Location = new System.Drawing.Point(347, 322);
+            this.button_OK.Location = new System.Drawing.Point(520, 483);
+            this.button_OK.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button_OK.Name = "button_OK";
-            this.button_OK.Size = new System.Drawing.Size(75, 23);
+            this.button_OK.Size = new System.Drawing.Size(112, 34);
             this.button_OK.TabIndex = 16;
             this.button_OK.Text = "确定";
             this.button_OK.UseVisualStyleBackColor = true;
@@ -175,9 +169,10 @@
             // 
             this.textBox_instanceName.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.textBox_instanceName.Location = new System.Drawing.Point(123, 322);
+            this.textBox_instanceName.Location = new System.Drawing.Point(184, 483);
+            this.textBox_instanceName.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox_instanceName.Name = "textBox_instanceName";
-            this.textBox_instanceName.Size = new System.Drawing.Size(163, 21);
+            this.textBox_instanceName.Size = new System.Drawing.Size(242, 28);
             this.textBox_instanceName.TabIndex = 15;
             this.textBox_instanceName.Text = "dp2kernel";
             // 
@@ -185,18 +180,20 @@
             // 
             this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(10, 325);
+            this.label4.Location = new System.Drawing.Point(15, 488);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(89, 12);
+            this.label4.Size = new System.Drawing.Size(134, 18);
             this.label4.TabIndex = 14;
             this.label4.Text = "内核实例名(&I):";
             // 
             // button_getSqlServerName
             // 
             this.button_getSqlServerName.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.button_getSqlServerName.Location = new System.Drawing.Point(219, 134);
+            this.button_getSqlServerName.Location = new System.Drawing.Point(328, 201);
+            this.button_getSqlServerName.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button_getSqlServerName.Name = "button_getSqlServerName";
-            this.button_getSqlServerName.Size = new System.Drawing.Size(202, 23);
+            this.button_getSqlServerName.Size = new System.Drawing.Size(303, 34);
             this.button_getSqlServerName.TabIndex = 12;
             this.button_getSqlServerName.Text = "获得邻近的SQL服务器名(&G)...";
             this.button_getSqlServerName.UseVisualStyleBackColor = true;
@@ -206,26 +203,39 @@
             // 
             this.textBox_sqlServerName.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.textBox_sqlServerName.Location = new System.Drawing.Point(123, 108);
+            this.textBox_sqlServerName.Location = new System.Drawing.Point(184, 162);
+            this.textBox_sqlServerName.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox_sqlServerName.Name = "textBox_sqlServerName";
-            this.textBox_sqlServerName.Size = new System.Drawing.Size(300, 21);
+            this.textBox_sqlServerName.Size = new System.Drawing.Size(448, 28);
             this.textBox_sqlServerName.TabIndex = 11;
             // 
             // label1
             // 
             this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(8, 110);
+            this.label1.Location = new System.Drawing.Point(12, 165);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(95, 12);
+            this.label1.Size = new System.Drawing.Size(143, 18);
             this.label1.TabIndex = 10;
             this.label1.Text = "SQL服务器名(&S):";
             // 
+            // checkBox_ssl
+            // 
+            this.checkBox_ssl.AutoSize = true;
+            this.checkBox_ssl.Location = new System.Drawing.Point(18, 208);
+            this.checkBox_ssl.Name = "checkBox_ssl";
+            this.checkBox_ssl.Size = new System.Drawing.Size(61, 22);
+            this.checkBox_ssl.TabIndex = 18;
+            this.checkBox_ssl.Text = "SSL";
+            this.checkBox_ssl.UseVisualStyleBackColor = true;
+            // 
             // MySqlDataSourceDlg
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(432, 385);
+            this.ClientSize = new System.Drawing.Size(648, 578);
+            this.Controls.Add(this.checkBox_ssl);
             this.Controls.Add(this.groupBox_login);
             this.Controls.Add(this.textBox_message);
             this.Controls.Add(this.button_Cancel);
@@ -235,6 +245,7 @@
             this.Controls.Add(this.button_getSqlServerName);
             this.Controls.Add(this.textBox_sqlServerName);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "MySqlDataSourceDlg";
             this.ShowIcon = false;
             this.ShowInTaskbar = false;
@@ -263,5 +274,6 @@
         private System.Windows.Forms.Button button_getSqlServerName;
         private System.Windows.Forms.TextBox textBox_sqlServerName;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox checkBox_ssl;
     }
 }
