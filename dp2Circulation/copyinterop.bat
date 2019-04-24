24ea513cd734e56767bad6a34fb346333da60998
xietao <xietao@dp2003.com>
2019-04-24 18:47:16 +0800

    整理代码

--- dp2Circulation/copyinterop.bat
+++ dp2Circulation/copyinterop.bat
@@ -5,7 +5,7 @@ MD x64
 
 CD ..\packages\system.data.sqlite.core.1.0.110.*
 
-copy .\build\net40\x86\sqlite.interop.dll ..\..\dp2circulation\x86
-copy .\build\net40\x64\sqlite.interop.dll ..\..\dp2circulation\x64
+copy .\build\net46\x86\sqlite.interop.dll ..\..\dp2circulation\x86
+copy .\build\net46\x64\sqlite.interop.dll ..\..\dp2circulation\x64
 
 CD ..\..\dp2circulation
