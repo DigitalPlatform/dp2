aa1a1f3b5ba399cd884767bfc9fabc064b408838
xietao <xietao@dp2003.com>
2019-06-25 01:33:41 +0800

    完善统计日志功能

---
+++ DigitalPlatform.LibraryServer/UidTable.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigitalPlatform.LibraryServer
+{
+    // 防止 UID 重复的 Hashtable
+    public class UidTable
+    {
+        public const int MAX_ITEMS = 1000;
+
+        Hashtable _table = new Hashtable();
+
+        object _syncRoot = new object();
+
+        public bool Contains(string uid)
+        {
+            lock (_syncRoot)
+            {
+                return _table.ContainsKey(uid);
+            }
+        }
+
+        public void Set(string uid)
+        {
+            lock (_syncRoot)
+            {
+                if (_table.Count > MAX_ITEMS)
+                    _table.Clear();
+
+                _table[uid] = true;
+            }
+        }
+    }
+}
