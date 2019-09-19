7b212d7de19b9ac3c809c2a8ea0e902f1d1ab318
xietao <xietao@dp2003.com>
2019-09-19 22:41:04 +0800

    增加视频显示功能

--- CallFaceCenterSample/Form1.Designer.cs
+++ CallFaceCenterSample/Form1.Designer.cs
@@ -29,6 +29,10 @@
         private void InitializeComponent()
         {
             this.button_faceRecognition1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button_startVideo = new System.Windows.Forms.Button();
+            this.button_stopVideo = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // button_faceRecognition1
@@ -41,16 +45,49 @@
             this.button_faceRecognition1.UseVisualStyleBackColor = true;
             this.button_faceRecognition1.Click += new System.EventHandler(this.button_faceRecognition1_Click);
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(13, 168);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(328, 270);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button_startVideo
+            // 
+            this.button_startVideo.Location = new System.Drawing.Point(371, 168);
+            this.button_startVideo.Name = "button_startVideo";
+            this.button_startVideo.Size = new System.Drawing.Size(144, 48);
+            this.button_startVideo.TabIndex = 2;
+            this.button_startVideo.Text = "开启视频";
+            this.button_startVideo.UseVisualStyleBackColor = true;
+            this.button_startVideo.Click += new System.EventHandler(this.button_startVideo_Click);
+            // 
+            // button_stopVideo
+            // 
+            this.button_stopVideo.Location = new System.Drawing.Point(371, 222);
+            this.button_stopVideo.Name = "button_stopVideo";
+            this.button_stopVideo.Size = new System.Drawing.Size(144, 48);
+            this.button_stopVideo.TabIndex = 3;
+            this.button_stopVideo.Text = "关闭视频";
+            this.button_stopVideo.UseVisualStyleBackColor = true;
+            this.button_stopVideo.Click += new System.EventHandler(this.button_stopVideo_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button_stopVideo);
+            this.Controls.Add(this.button_startVideo);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.button_faceRecognition1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -58,6 +95,9 @@
         #endregion
 
         private System.Windows.Forms.Button button_faceRecognition1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button_startVideo;
+        private System.Windows.Forms.Button button_stopVideo;
     }
 }
 
