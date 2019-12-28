ec8bc73b81918ea181a1b0fe76221d865e5ac592
xietao <xietao@dp2003.com>
2019-12-28 13:59:26 +0800

    添加 CallRfidCenterSample

---
+++ CallRfidCenterSample/Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace CallRfidCenterSample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button_tagListForm = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button_tagListForm
+            // 
+            this.button_tagListForm.Location = new System.Drawing.Point(13, 13);
+            this.button_tagListForm.Name = "button_tagListForm";
+            this.button_tagListForm.Size = new System.Drawing.Size(422, 40);
+            this.button_tagListForm.TabIndex = 0;
+            this.button_tagListForm.Text = "打开 TagListForm";
+            this.button_tagListForm.UseVisualStyleBackColor = true;
+            this.button_tagListForm.Click += new System.EventHandler(this.button_tagListForm_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button_tagListForm);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_tagListForm;
+    }
+}
+
---
+++ CallRfidCenterSample/TagListForm.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using DigitalPlatform.GUI;
+using DigitalPlatform.RFID;
+
+namespace CallRfidCenterSample
+{
+    public partial class TagListForm : Form
+    {
+        public TagListForm()
+        {
+            InitializeComponent();
+        }
+
+        private void TagListForm_Load(object sender, EventArgs e)
+        {
+            Form1.TagChanged += Form1_TagChanged;
+
+            this.Invoke((Action)(() =>
+            {
+                foreach (var tag in TagList.Books)
+                {
+                    UpdateItem(tag);
+                }
+
+                foreach (var tag in TagList.Patrons)
+                {
+                    UpdateItem(tag);
+                }
+            }));
+        }
+
+        private void Form1_TagChanged(object sender, TagChangedEventArgs e)
+        {
+            this.Invoke((Action)(() =>
+            {
+                if (e.AddBooks != null)
+                    foreach (var tag in e.AddBooks)
+                    {
+                        AddItem(tag);
+                    }
+
+                if (e.UpdateBooks != null)
+                    foreach (var tag in e.UpdateBooks)
+                    {
+                        UpdateItem(tag);
+                    }
+
+                if (e.RemoveBooks != null)
+                    foreach (var tag in e.RemoveBooks)
+                    {
+                        RemoveItem(tag);
+                    }
+
+                if (e.AddPatrons != null)
+                    foreach (var tag in e.AddPatrons)
+                    {
+                        AddItem(tag);
+                    }
+
+                if (e.UpdatePatrons != null)
+                    foreach (var tag in e.UpdatePatrons)
+                    {
+                        UpdateItem(tag);
+                    }
+
+                if (e.RemovePatrons != null)
+                    foreach (var tag in e.RemovePatrons)
+                    {
+                        RemoveItem(tag);
+                    }
+            }));
+        }
+
+        private void TagListForm_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Form1.TagChanged -= Form1_TagChanged;
+        }
+
+        const int COLUMN_UID = 0;
+        const int COLUMN_PII = 1;
+        const int COLUMN_PROTOCOL = 2;
+        const int COLUMN_ANTENNA = 3;
+
+        // 添加一个新行
+        ListViewItem AddItem(TagAndData tag)
+        {
+            ListViewItem item = new ListViewItem();
+            this.listView_tags.Items.Add(item);
+            RefreshItem(item, tag);
+            return item;
+        }
+
+        void RefreshItem(ListViewItem item, TagAndData tag)
+        {
+            string pii = "";
+
+            var taginfo = tag.OneTag.TagInfo;
+            if (taginfo != null)
+            {
+                // Exception:
+                //      可能会抛出异常 ArgumentException TagDataException
+                var chip = LogicChip.From(taginfo.Bytes,
+    (int)taginfo.BlockSize,
+    "");
+                pii = chip.FindElement(ElementOID.PII)?.Text;
+            }
+
+            ListViewUtil.ChangeItemText(item, COLUMN_UID, tag.OneTag.UID);
+            ListViewUtil.ChangeItemText(item, COLUMN_PII, pii);
+            ListViewUtil.ChangeItemText(item, COLUMN_PROTOCOL, tag.OneTag.Protocol);
+            ListViewUtil.ChangeItemText(item, COLUMN_ANTENNA, tag.OneTag.AntennaID.ToString());
+        }
+
+        // 移走一行
+        void RemoveItem(TagAndData tag)
+        {
+            var item = ListViewUtil.FindItem(this.listView_tags, tag.OneTag.UID, COLUMN_UID);
+            if (item == null)
+                return;
+            this.listView_tags.Items.Remove(item);
+        }
+
+        // 更新一行内容
+        void UpdateItem(TagAndData tag)
+        {
+            var item = ListViewUtil.FindItem(this.listView_tags, tag.OneTag.UID, COLUMN_UID);
+            if (item == null)
+            {
+                AddItem(tag);
+                return;
+            }
+            RefreshItem(item, tag);
+        }
+    }
+}
