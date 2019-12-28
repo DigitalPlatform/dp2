458b4920ec4ca5450f55c446641b6ee4362baa7a
xietao <xietao@dp2003.com>
2019-12-28 13:59:26 +0800

    添加 CallRfidCenterSample

---
+++ CallRfidCenterSample/TagListForm.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace CallRfidCenterSample
+{
+    partial class TagListForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView_tags = new System.Windows.Forms.ListView();
+            this.columnHeader_uid = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader_pii = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader_protocol = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader_antenna = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // listView_tags
+            // 
+            this.listView_tags.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listView_tags.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader_uid,
+            this.columnHeader_pii,
+            this.columnHeader_protocol,
+            this.columnHeader_antenna});
+            this.listView_tags.HideSelection = false;
+            this.listView_tags.Location = new System.Drawing.Point(13, 13);
+            this.listView_tags.Name = "listView_tags";
+            this.listView_tags.Size = new System.Drawing.Size(775, 375);
+            this.listView_tags.TabIndex = 0;
+            this.listView_tags.UseCompatibleStateImageBehavior = false;
+            this.listView_tags.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader_uid
+            // 
+            this.columnHeader_uid.Text = "UID";
+            this.columnHeader_uid.Width = 144;
+            // 
+            // columnHeader_pii
+            // 
+            this.columnHeader_pii.Text = "PII";
+            this.columnHeader_pii.Width = 221;
+            // 
+            // columnHeader_protocol
+            // 
+            this.columnHeader_protocol.Text = "协议";
+            this.columnHeader_protocol.Width = 178;
+            // 
+            // columnHeader_antenna
+            // 
+            this.columnHeader_antenna.Text = "天线编号";
+            this.columnHeader_antenna.Width = 100;
+            // 
+            // TagListForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listView_tags);
+            this.Name = "TagListForm";
+            this.Text = "TagListForm";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.TagListForm_FormClosed);
+            this.Load += new System.EventHandler(this.TagListForm_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView_tags;
+        private System.Windows.Forms.ColumnHeader columnHeader_uid;
+        private System.Windows.Forms.ColumnHeader columnHeader_pii;
+        private System.Windows.Forms.ColumnHeader columnHeader_protocol;
+        private System.Windows.Forms.ColumnHeader columnHeader_antenna;
+    }
+}
