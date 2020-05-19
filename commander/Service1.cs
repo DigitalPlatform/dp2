87d5b1b8fb5b44040c24880bf530c383d15b10cb
谢涛 <xietao@dp2003.com>
2020-05-19 11:39:03 +0800

    开始添加 Commander Project

---
+++ commander/Service1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace commander
+{
+    partial class Service1 : ServiceBase
+    {
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            // TODO: 在此处添加代码以启动服务。
+        }
+
+        protected override void OnStop()
+        {
+            // TODO: 在此处添加代码以执行停止服务所需的关闭操作。
+        }
+    }
+}
