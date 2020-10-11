dcc9db88f7e7de82f0c56f383b39b95bc20274eb
谢涛 <xietao@dp2003.com>
2020-10-11 22:10:35 +0800

    整理代码

--- dp2SSL/Models/InventoryData.cs
+++ dp2SSL/Models/InventoryData.cs
@@ -7,7 +7,10 @@ using System.Threading;
 using System.Threading.Tasks;
 
 using DigitalPlatform;
+using DigitalPlatform.LibraryClient;
+using DigitalPlatform.LibraryClient.localhost;
 using DigitalPlatform.RFID;
+using DigitalPlatform.Text;
 using DigitalPlatform.WPF;
 
 namespace dp2SSL
@@ -20,6 +23,11 @@ namespace dp2SSL
         // 预先从全部实体记录中准备好 UID 到 PII 的对照关系。这一部分标签就不需要 GetTagData 了
         // UID --> PII
         static Hashtable _uidTable = new Hashtable();
+
+        public static void SetUidTable(Hashtable table)
+        {
+            _uidTable = table;
+        }
 
         // 检查是否存在 UID --> PII 事项
         public static bool UidExsits(string uid, out string pii)
@@ -58,6 +66,109 @@ namespace dp2SSL
             _entityTable[entity.UID] = entity;
             isNewly = true;
             return entity;
+        }
+
+        public static void UpdateEntity(Entity entity, TagInfo tagInfo)
+        {
+            entity.TagInfo = tagInfo;
+
+            bool throw_exception = false;
+            LogicChip chip = null;
+            string type = "";
+            if (string.IsNullOrEmpty(type))
+            {
+                // Exception:
+                //      可能会抛出异常 ArgumentException TagDataException
+                try
+                {
+                    ParseTagInfo(tagInfo,
+out string pii,
+out type,
+out chip);
+                    if (tagInfo != null)
+                        entity.PII = pii;
+                }
+                catch (Exception ex)
+                {
+                    App.CurrentApp.SpeakSequence("警告: 标签解析出错");
+                    if (throw_exception == false)
+                    {
+                        entity.AppendError($"RFID 标签格式错误: {ex.Message}",
+                            "red",
+                            "parseTagError");
+                    }
+                    else
+                        throw ex;
+                }
+            }
+
+            // 2020/4/9
+            if (type == "patron")
+            {
+                // 避免被当作图书同步到 dp2library
+                entity.PII = "(读者卡)" + entity.PII;
+                entity.AppendError("读者卡误放入书柜", "red", "patronCard");
+            }
+
+            // 2020/7/15
+            // 获得图书 RFID 标签的 OI 和 AOI 字段
+            if (type == "book")
+            {
+                if (chip == null)
+                {
+                    // Exception:
+                    //      可能会抛出异常 ArgumentException TagDataException
+                    chip = LogicChip.From(tagInfo.Bytes,
+            (int)tagInfo.BlockSize,
+            "" // tag.TagInfo.LockStatus
+            );
+                }
+
+                if (chip.IsBlank())
+                {
+                    entity.AppendError("空白标签", "red", "blankTag");
+                }
+                else
+                {
+                    string oi = chip.FindElement(ElementOID.OI)?.Text;
+                    string aoi = chip.FindElement(ElementOID.AOI)?.Text;
+
+                    entity.OI = oi;
+                    entity.AOI = aoi;
+
+                    // 2020/8/27
+                    // 严格要求必须有 OI(AOI) 字段
+                    if (string.IsNullOrEmpty(oi) && string.IsNullOrEmpty(aoi))
+                        entity.AppendError("没有 OI 或 AOI 字段", "red", "missingOI");
+                }
+            }
+        }
+
+        static void ParseTagInfo(TagInfo tagInfo,
+    out string pii,
+    out string type,
+    out LogicChip chip)
+        {
+            pii = null;
+            chip = null;
+            type = "";
+
+            if (tagInfo == null)
+                return;
+
+            // Exception:
+            //      可能会抛出异常 ArgumentException TagDataException
+            chip = LogicChip.From(tagInfo.Bytes,
+    (int)tagInfo.BlockSize,
+    "" // tag.TagInfo.LockStatus
+    );
+            pii = chip.FindElement(ElementOID.PII)?.Text;
+
+            var typeOfUsage = chip.FindElement(ElementOID.TypeOfUsage)?.Text;
+            if (typeOfUsage != null && typeOfUsage.StartsWith("8"))
+                type = "patron";
+            else
+                type = "book";
         }
 
         // 注：所创建的 Entity 对象其 Error 成员可能有值，表示有出错信息
@@ -132,16 +243,23 @@ namespace dp2SSL
             );
                 }
 
-                string oi = chip.FindElement(ElementOID.OI)?.Text;
-                string aoi = chip.FindElement(ElementOID.AOI)?.Text;
+                if (chip.IsBlank())
+                {
+                    entity.AppendError("空白标签", "red", "blankTag");
+                }
+                else
+                {
+                    string oi = chip.FindElement(ElementOID.OI)?.Text;
+                    string aoi = chip.FindElement(ElementOID.AOI)?.Text;
 
-                result.OI = oi;
-                result.AOI = aoi;
+                    result.OI = oi;
+                    result.AOI = aoi;
 
-                // 2020/8/27
-                // 严格要求必须有 OI(AOI) 字段
-                if (string.IsNullOrEmpty(oi) && string.IsNullOrEmpty(aoi))
-                    result.AppendError("没有 OI 或 AOI 字段", "red", "missingOI");
+                    // 2020/8/27
+                    // 严格要求必须有 OI(AOI) 字段
+                    if (string.IsNullOrEmpty(oi) && string.IsNullOrEmpty(aoi))
+                        result.AppendError("没有 OI 或 AOI 字段", "red", "missingOI");
+                }
             }
             return result;
         }
@@ -191,6 +309,9 @@ namespace dp2SSL
         {
             // 状态
             public string State { get; set; }
+
+            // GetTagInfo() 出错的次数
+            public int ErrorCount { get; set; }
         }
 
         #region 处理列表
@@ -233,9 +354,19 @@ namespace dp2SSL
             }
         }
 
+
+
         #region GetTagInfo() 后出错状态的 Entity 集合
 
         static List<Entity> _errorEntities = new List<Entity>();
+
+        public static List<Entity> ErrorEntities
+        {
+            get
+            {
+                return new List<Entity>(_errorEntities);
+            }
+        }
 
         public static int AddErrorEntity(Entity entity, out bool changed)
         {
@@ -358,5 +489,169 @@ TaskScheduler.Default);
         }
 
         #endregion
+
+        public delegate void delegate_showText(string text);
+
+        // parameters:
+        //      uid_table   返回 UID --> PII 对照表
+        public static NormalResult DownloadUidTable(
+            List<string> item_dbnames,
+            Hashtable uid_table,
+            delegate_showText func_showProgress,
+            // Delegate_writeLog writeLog,
+            CancellationToken token)
+        {
+            WpfClientInfo.WriteInfoLog($"开始下载全部册记录到本地缓存");
+            LibraryChannel channel = App.CurrentApp.GetChannel();
+            var old_timeout = channel.Timeout;
+            channel.Timeout = TimeSpan.FromMinutes(5);  // 设置 5 分钟。因为册记录检索需要一定时间
+            try
+            {
+                if (item_dbnames == null)
+                {
+                    long lRet = channel.GetSystemParameter(
+    null,
+    "item",
+    "dbnames",
+    out string strValue,
+    out string strError);
+                    if (lRet == -1)
+                        return new NormalResult
+                        {
+                            Value = -1,
+                            ErrorInfo = strError,
+                            ErrorCode = channel.ErrorCode.ToString()
+                        };
+                    item_dbnames = StringUtil.SplitList(strValue);
+                    StringUtil.RemoveBlank(ref item_dbnames);
+                }
+
+                foreach (string dbName in item_dbnames)
+                {
+                    func_showProgress?.Invoke($"正在从 {dbName} 获取信息 ...");
+
+                    int nRedoCount = 0;
+                REDO:
+                    if (token.IsCancellationRequested)
+                        return new NormalResult
+                        {
+                            Value = -1,
+                            ErrorInfo = "用户中断"
+                        };
+                    // 检索全部读者库记录
+                    long lRet = channel.SearchItem(null,
+    dbName, // "<all>",
+    "",
+    -1,
+    "__id",
+    "left",
+    "zh",
+    null,   // strResultSetName
+    "", // strSearchStyle
+    "", // strOutputStyle
+    out string strError);
+                    if (lRet == -1)
+                    {
+                        WpfClientInfo.WriteErrorLog($"SearchItem() 出错, strError={strError}, channel.ErrorCode={channel.ErrorCode}");
+
+                        // 一次重试机会
+                        if (lRet == -1
+                            && (channel.ErrorCode == ErrorCode.RequestCanceled || channel.ErrorCode == ErrorCode.RequestError)
+                            && nRedoCount < 2)
+                        {
+                            nRedoCount++;
+                            goto REDO;
+                        }
+
+                        return new NormalResult
+                        {
+                            Value = -1,
+                            ErrorInfo = strError,
+                            ErrorCode = channel.ErrorCode.ToString()
+                        };
+                    }
+
+                    long hitcount = lRet;
+
+                    WpfClientInfo.WriteInfoLog($"{dbName} 共检索命中册记录 {hitcount} 条");
+
+                    // 把超时时间改短一点
+                    channel.Timeout = TimeSpan.FromSeconds(20);
+
+                    DateTime search_time = DateTime.Now;
+
+                    int skip_count = 0;
+                    int error_count = 0;
+
+                    if (hitcount > 0)
+                    {
+                        string strStyle = "id,cols,format:@coldef:*/barcode|*/location|*/uid";
+
+                        // 获取和存储记录
+                        ResultSetLoader loader = new ResultSetLoader(channel,
+            null,
+            null,
+            strStyle,   // $"id,xml,timestamp",
+            "zh");
+
+                        // loader.Prompt += this.Loader_Prompt;
+                        int i = 0;
+                        foreach (DigitalPlatform.LibraryClient.localhost.Record record in loader)
+                        {
+                            if (token.IsCancellationRequested)
+                                return new NormalResult
+                                {
+                                    Value = -1,
+                                    ErrorInfo = "用户中断"
+                                };
+
+                            if (record.Cols != null)
+                            {
+                                string barcode = "";
+                                if (record.Cols.Length > 0)
+                                    barcode = record.Cols[0];
+                                string location = "";
+                                if (record.Cols.Length > 1)
+                                    location = record.Cols[1];
+                                string uid = "";
+                                if (record.Cols.Length > 2)
+                                    uid = record.Cols[2];
+                                if (string.IsNullOrEmpty(barcode) == false
+                                    && string.IsNullOrEmpty(uid) == false)
+                                    uid_table[uid] = barcode;
+                            }
+
+                            i++;
+                        }
+
+                    }
+
+                    WpfClientInfo.WriteInfoLog($"dbName='{dbName}'。skip_count={skip_count}, error_count={error_count}");
+
+                }
+                return new NormalResult
+                {
+                    Value = uid_table.Count,
+                };
+            }
+            catch (Exception ex)
+            {
+                WpfClientInfo.WriteErrorLog($"DownloadItemRecordAsync() 出现异常：{ExceptionUtil.GetDebugText(ex)}");
+
+                return new NormalResult
+                {
+                    Value = -1,
+                    ErrorInfo = $"DownloadItemRecordAsync() 出现异常：{ex.Message}"
+                };
+            }
+            finally
+            {
+                channel.Timeout = old_timeout;
+                App.CurrentApp.ReturnChannel(channel);
+
+                WpfClientInfo.WriteInfoLog($"结束下载全部读者记录到本地缓存");
+            }
+        }
+
     }
 }
--- dp2SSL/Page/PageInventory.xaml.cs
+++ dp2SSL/Page/PageInventory.xaml.cs
@@ -18,6 +18,9 @@ using static dp2SSL.InventoryData;
 
 using DigitalPlatform;
 using DigitalPlatform.RFID;
+using DigitalPlatform.Text;
+using System.Collections;
+using DigitalPlatform.WPF;
 
 namespace dp2SSL
 {
@@ -74,6 +77,59 @@ namespace dp2SSL
         {
             App.NewTagChanged += CurrentApp_NewTagChanged;
             App.IsPageInventoryActive = true;
+
+            _ = Task.Run(() =>
+            {
+                CancellationTokenSource cancel = new CancellationTokenSource();
+
+                ProgressWindow progress = null;
+                App.Invoke(new Action(() =>
+                {
+                    progress = new ProgressWindow();
+                    progress.TitleText = "dp2SSL -- 盘点";
+                    progress.MessageText = "正在获取 UID 对照信息，请稍候 ...";
+                    progress.Owner = Application.Current.MainWindow;
+                    progress.WindowStartupLocation = WindowStartupLocation.CenterOwner;
+                    progress.Closed += (s, e1) =>
+                    {
+                        cancel.Cancel();
+                    };
+                    progress.okButton.Visibility = Visibility.Collapsed;
+                    // progress.okButton.Content = "停止";
+                    App.SetSize(progress, "middle");
+                    progress.BackColor = "green";
+                    progress.Show();
+                }));
+
+                try
+                {
+                    Hashtable uid_table = new Hashtable();
+                    var result = InventoryData.DownloadUidTable(
+            null,
+            uid_table,
+            (text) =>
+            {
+                App.Invoke(new Action(() =>
+                {
+                    progress.MessageText = text;
+                }));
+            },
+            cancel.Token);
+                    InventoryData.SetUidTable(uid_table);
+                }
+                catch (Exception ex)
+                {
+                    WpfClientInfo.WriteErrorLog($"准备册记录过程出现异常: {ExceptionUtil.GetDebugText(ex)}");
+                }
+                finally
+                {
+                    App.Invoke(new Action(() =>
+                    {
+                        progress.Close();
+                    }));
+                }
+            });
+
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
@@ -89,6 +145,7 @@ namespace dp2SSL
             this.NavigationService.Navigate(PageMenu.MenuPage);
         }
 
+#if NO
         int _tagChangedCount = 0;
 
         // 首次初始化 Entity 列表
@@ -106,7 +163,7 @@ namespace dp2SSL
 
             return new NormalResult();
         }
-
+#endif
 
         // 新版本的事件
 #pragma warning disable VSTHRD100 // 避免使用 Async Void 方法
@@ -128,6 +185,26 @@ namespace dp2SSL
             var channel = (BaseChannel<IRfid>)sender;
             // TODO: 对离开的 tag 变化为灰色颜色
 
+            bool speaked = false;
+            // 2020/10/10
+            // TODO: 发出什么响声表示?
+            // 把以前遗留的出错 entity 尝试重新 GetTagInfo()
+            foreach (var entity in InventoryData.ErrorEntities)
+            {
+                FillEntity(channel, entity, (e1) =>
+                {
+                    // 说过一次便不再说
+                    if (speaked == true)
+                        return false;
+                    speaked = SpeakLocation(e1);
+                    return speaked;
+                });
+            }
+
+            // 筛选出需要 GetTagInfo() 的那些标签
+            FilterTags(channel, e.AddTags);
+
+#if NO
             SoundMaker.InitialSequence(e.AddTags.Count);
 
             foreach (var tag in e.AddTags)
@@ -137,6 +214,7 @@ namespace dp2SSL
             }
 
             SoundMaker.StopCurrent();
+#endif
 
             /*
             foreach (var tag in e.UpdateTags)
@@ -146,6 +224,75 @@ namespace dp2SSL
             */
         }
 
+        // 筛选出需要 GetTagInfo() 的那些标签
+        void FilterTags(BaseChannel<IRfid> channel, List<TagAndData> tags)
+        {
+            List<Entity> entities1 = new List<Entity>();
+            List<Entity> all = new List<Entity>();
+
+            foreach (var tag in tags)
+            {
+                var entity = InventoryData.AddEntity(tag, out bool isNewly);
+                var info = entity.Tag as ProcessInfo;
+                if (info == null)
+                {
+                    info = new ProcessInfo();
+                    entity.Tag = info;
+                }
+
+                if (isNewly)
+                {
+                    App.Invoke(new Action(() =>
+                    {
+                        _entities.Add(entity);
+                    }));
+                }
+
+                if (string.IsNullOrEmpty(entity.PII))
+                    entities1.Add(entity);
+
+                all.Add(entity);
+            }
+
+            // 准备音阶
+            SoundMaker.InitialSequence(entities1.Count);
+
+            bool speaked = false;
+            // 集中 GetTagInfo()
+            foreach (var entity in entities1)
+            {
+                SoundMaker.NextSound();
+
+                FillEntity(channel, entity,
+                    (e) =>
+                    {
+                        // 说过一次便不再说
+                        if (speaked == true)
+                            return false;
+                        speaked = SpeakLocation(e);
+                        return speaked;
+                    });
+            }
+
+            // 停止音阶响声
+            SoundMaker.StopCurrent();
+
+            // 获取题名等
+            foreach (var entity in all)
+            {
+                var info = entity.Tag as ProcessInfo;
+
+                if (string.IsNullOrEmpty(entity.PII) == false
+&& string.IsNullOrEmpty(info.State))
+                {
+                    info.State = "processing";  // 正在获取册信息
+                    InventoryData.AppendList(entity);
+                    InventoryData.ActivateInventory();
+                }
+            }
+        }
+
+#if NO
         void ProcessTag(BaseChannel<IRfid> channel, TagAndData tag)
         {
             var entity = InventoryData.AddEntity(tag, out bool isNewly);
@@ -164,6 +311,17 @@ namespace dp2SSL
                     _entities.Add(entity);
                 }));
             }
+
+            FillEntity(channel, entity);
+        }
+#endif
+        delegate bool delegate_speakLocation(Entity entity);
+
+        void FillEntity(BaseChannel<IRfid> channel,
+            Entity entity,
+            delegate_speakLocation func_speakLocation)
+        {
+            var info = entity.Tag as ProcessInfo;
 
             // 2020/10/7
             // 尝试获取 PII
@@ -198,26 +356,35 @@ namespace dp2SSL
                                 App.CurrentApp.SpeakSequence(count.ToString());
 
                             info.State = "errorGetTagInfo";
+                            info.ErrorCount++;
                             error = get_result.ErrorInfo;
 
                             // TODO: 当有一行以上 GetTagInfo() 出错时，要不断发出响声警告。
-                        
+                            entity.Error = error;
+
+                            if (info.ErrorCount > 5)
+                            {
+                                if (func_speakLocation?.Invoke(entity) == true)
+                                    info.ErrorCount = 0;
+                            }
                         }
                         else
                         {
+                            entity.Error = null;
+
                             // 把 PII 显示出来
-                            tag.OneTag.TagInfo = get_result.TagInfo;
-                            InventoryData.AddEntity(tag, out isNewly);
+                            InventoryData.UpdateEntity(entity, get_result.TagInfo);
                             info.State = "";
 
                             // 朗读出错 entity 数量
                             var count = InventoryData.RemoveErrorEntity(entity, out bool changed);
                             if (changed == true)
                                 App.CurrentApp.SpeakSequence(count.ToString());
+
+                            if (StringUtil.IsInList("blankTag", entity.ErrorCode))
+                                App.CurrentApp.SpeakSequence(entity.Error);
                         }
                     }
-
-                    entity.Error = error;
                 }
             }
 
@@ -228,6 +395,97 @@ namespace dp2SSL
                 InventoryData.AppendList(entity);
                 InventoryData.ActivateInventory();
             }
+        }
+
+        string FindTitle(int index)
+        {
+            int prev_index = index;
+            int next_index = index;
+
+            while (true)
+            {
+                prev_index--;
+                next_index++;
+                if (IsOut(prev_index))
+                    break;
+                Entity prev = _entities[prev_index];
+                if (string.IsNullOrEmpty(prev.Title) == false)
+                    return prev.Title;
+                if (IsOut(next_index))
+                    break;
+                Entity next = _entities[next_index];
+                if (string.IsNullOrEmpty(next.Title) == false)
+                    return next.Title;
+            }
+
+            return null;
+
+            bool IsOut(int i)
+            {
+                if (i < 0 || i > _entities.Count - 1)
+                    return true;
+                return false;
+            }
+        }
+
+        // 语音播报提醒 entity 的位置
+        bool SpeakLocation(Entity entity)
+        {
+            int index = _entities.IndexOf(entity);
+            if (index == -1)
+                return false;
+
+            string title = FindTitle(index);
+            if (title == null)
+                return false;
+
+            App.CurrentApp.Speak($"在 {CutTitle(title)} 附近");
+            return true;
+        }
+
+
+#if NO
+        // 语音播报提醒 entity 的位置
+        void SpeakLocation(Entity entity)
+        {
+            int index = _entities.IndexOf(entity);
+            if (index == -1)
+                return;
+            Entity prev = null;
+            if (index > 0)
+                prev = _entities[index-1];
+
+            string prev_title = CutTitle(prev?.Title);
+
+            Entity next = null;
+            if (index < _entities.Count - 1)
+                next = _entities[index++];
+
+            string next_title = CutTitle(next?.Title);
+
+            if (prev != null && next != null)
+                App.CurrentApp.Speak($"在 {prev_title} 和 {next_title} 之间");
+            else if (prev == null && next != null)
+                App.CurrentApp.Speak($"在 {next_title} 之前");
+            else if (prev != null && next == null)
+                App.CurrentApp.Speak($"在 {prev_title} 之后");
+        }
+
+#endif
+
+        static string CutTitle(string title)
+        {
+            if (title == null)
+                return null;
+
+            int index = title.IndexOf("/");
+            if (index != -1)
+                title = title.Substring(0, index).Trim();
+
+            if (title.Length > 20)
+                return title.Substring(0, 20);
+
+            return title;
         }
 
         private void clearList_Click(object sender, RoutedEventArgs e)
