94bbaa8ba327174faa2a5e877ea394cd01f70f29
谢涛 <xietao@dp2003.com>
2020-10-12 23:20:42 +0800

    开始实现典藏移交清单统计打印功能

---
+++ dp2Circulation/OperLogStatis/SelectOutputRangeDialog.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace dp2Circulation
+{
+    partial class SelectOutputRangeDialog
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button_OK = new System.Windows.Forms.Button();
+            this.button_Cancel = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader_batchNo = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader_targetLocation = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader_itemCount = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // button_OK
+            // 
+            this.button_OK.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button_OK.Location = new System.Drawing.Point(574, 399);
+            this.button_OK.Margin = new System.Windows.Forms.Padding(4);
+            this.button_OK.Name = "button_OK";
+            this.button_OK.Size = new System.Drawing.Size(103, 38);
+            this.button_OK.TabIndex = 6;
+            this.button_OK.Text = "确定";
+            this.button_OK.UseVisualStyleBackColor = true;
+            this.button_OK.Click += new System.EventHandler(this.button_OK_Click);
+            // 
+            // button_Cancel
+            // 
+            this.button_Cancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button_Cancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.button_Cancel.Location = new System.Drawing.Point(684, 399);
+            this.button_Cancel.Margin = new System.Windows.Forms.Padding(4);
+            this.button_Cancel.Name = "button_Cancel";
+            this.button_Cancel.Size = new System.Drawing.Size(103, 38);
+            this.button_Cancel.TabIndex = 7;
+            this.button_Cancel.Text = "取消";
+            this.button_Cancel.UseVisualStyleBackColor = true;
+            this.button_Cancel.Click += new System.EventHandler(this.button_Cancel_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listView1.CheckBoxes = true;
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader_batchNo,
+            this.columnHeader_targetLocation,
+            this.columnHeader_itemCount});
+            this.listView1.FullRowSelect = true;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(13, 13);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(775, 379);
+            this.listView1.TabIndex = 8;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader_batchNo
+            // 
+            this.columnHeader_batchNo.Text = "批次号";
+            this.columnHeader_batchNo.Width = 300;
+            // 
+            // columnHeader_targetLocation
+            // 
+            this.columnHeader_targetLocation.Text = "目标馆藏地";
+            this.columnHeader_targetLocation.Width = 200;
+            // 
+            // columnHeader_itemCount
+            // 
+            this.columnHeader_itemCount.Text = "册数";
+            this.columnHeader_itemCount.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.columnHeader_itemCount.Width = 100;
+            // 
+            // SelectOutputRangeDialog
+            // 
+            this.AcceptButton = this.button_OK;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.button_Cancel;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.button_OK);
+            this.Controls.Add(this.button_Cancel);
+            this.Name = "SelectOutputRangeDialog";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.Text = "选择输出范围";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_OK;
+        private System.Windows.Forms.Button button_Cancel;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader_batchNo;
+        private System.Windows.Forms.ColumnHeader columnHeader_targetLocation;
+        private System.Windows.Forms.ColumnHeader columnHeader_itemCount;
+    }
+}
---
+++ dp2Circulation/OperLogStatis/SelectOutputRangeDialog.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using DigitalPlatform.GUI;
+
+namespace dp2Circulation
+{
+    public partial class SelectOutputRangeDialog : Form
+    {
+        public SelectOutputRangeDialog()
+        {
+            InitializeComponent();
+        }
+
+        private void button_OK_Click(object sender, EventArgs e)
+        {
+            this.DialogResult = DialogResult.OK;
+            this.Close();
+        }
+
+        private void button_Cancel_Click(object sender, EventArgs e)
+        {
+            this.DialogResult = DialogResult.Cancel;
+            this.Close();
+        }
+
+        List<TransferGroup> GetGroups(IEnumerable<ListViewItem> items)
+        {
+            List<TransferGroup> results = new List<TransferGroup>();
+            foreach (ListViewItem item in items)
+            {
+                var result = new TransferGroup();
+                result.BatchNo = ListViewUtil.GetItemText(item, 0);
+                result.TargetLocation = ListViewUtil.GetItemText(item, 1);
+                result.Items = item.Tag as List<TransferItem>;
+                results.Add(result);
+            }
+
+            return results;
+        }
+
+        public List<TransferGroup> SelectedGroups
+        {
+            get
+            {
+                return GetGroups(this.listView1.CheckedItems.Cast<ListViewItem>());
+            }
+        }
+
+        public List<TransferGroup> Groups
+        {
+            get
+            {
+                return GetGroups(this.listView1.Items.Cast<ListViewItem>());
+
+                /*
+                List<TransferGroup> results = new List<TransferGroup>();
+                foreach(ListViewItem item in this.listView1.Items)
+                {
+                    var result = new TransferGroup();
+                    result.BatchNo = ListViewUtil.GetItemText(item, 0);
+                    result.TargetLocation = ListViewUtil.GetItemText(item, 1);
+                    result.Items = item.Tag as List<TransferItem>;
+                    results.Add(result);
+                }
+
+                return results;
+                */
+            }
+            set
+            {
+                this.listView1.Items.Clear();
+                foreach(var group in value)
+                {
+                    ListViewItem item = new ListViewItem();
+                    ListViewUtil.ChangeItemText(item, 0, group.BatchNo);
+                    ListViewUtil.ChangeItemText(item, 1, group.TargetLocation);
+                    ListViewUtil.ChangeItemText(item, 2, group.Items.Count.ToString());
+                    item.Tag = group.Items;
+
+                    this.listView1.Items.Add(item);
+                }
+            }
+        }
+    }
+
+    // 一组 TransferItem
+    public class TransferGroup
+    {
+        public string BatchNo { get; set; }
+        public string TargetLocation { get; set; }
+        public List<TransferItem> Items { get; set; }
+    }
+
+    // 典藏移交动作
+    public class TransferItem
+    {
+        // 册记录路径
+        public string RecPath { get; set; }
+        // 册条码号
+        public string Barcode { get; set; }
+        // 批次号
+        public string BatchNo { get; set; }
+        // 源 location
+        public string SourceLocation { get; set; }
+        // 目标 location
+        public string TargetLocation { get; set; }
+    }
+}
