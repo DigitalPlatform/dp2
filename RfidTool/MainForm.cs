32a0215475976067e9294cfe5e1988f1cd134642
谢涛 <xietao@dp2003.com>
2020-11-28 00:24:00 +0800

    整理代码

--- RfidTool/MainForm.cs
+++ RfidTool/MainForm.cs
@@ -12,20 +12,56 @@ namespace RfidTool
 {
     public partial class MainForm : Form
     {
+        ScanDialog _scanDialog = new ScanDialog();
+
         public MainForm()
         {
             InitializeComponent();
+
+            this.MenuItem_exit.Click += MenuItem_exit_Click;
+            this.MenuItem_writeBookTags.Click += MenuItem_writeBookTags_Click;
+
+            _scanDialog.FormClosing += _scanDialog_FormClosing;
         }
 
+        private void _scanDialog_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            var dialog = sender as Form;
+
+            // 将关闭改为隐藏
+            dialog.Visible = false;
+            if (e.CloseReason == CloseReason.UserClosing)
+                e.Cancel = true;
+        }
+
+        // 开始(扫描并)写入图书标签
         private void MenuItem_writeBookTags_Click(object sender, System.EventArgs e)
         {
-            throw new System.NotImplementedException();
+            // 把扫描对话框打开
+            if (_scanDialog.Visible == false)
+                _scanDialog.Show(this);
+
         }
 
+        // 退出
         private void MenuItem_exit_Click(object sender, System.EventArgs e)
         {
-            throw new System.NotImplementedException();
+            this.Close();
         }
 
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            DataModel.StartRfidManager("ipc://RfidChannel/RfidServer");
+        }
+
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            DataModel.StopRfidManager();
+        }
+
+        private void MainForm_FormClosed(object sender, FormClosedEventArgs e)
+        {
+
+        }
     }
 }
