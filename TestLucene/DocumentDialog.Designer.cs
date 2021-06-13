dd07eed9788bb90c3fdd5918f83af88fbe966622
谢涛 <xietao@dp2003.com>
2021-06-13 23:17:32 +0800

    整理代码

---
+++ TestLucene/DocumentDialog.Designer.cs
@@ -0,0 +1,194 @@
+﻿
+namespace TestLucene
+{
+    partial class DocumentDialog
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox_id = new System.Windows.Forms.TextBox();
+            this.textBox_title = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox_author = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button_OK = new System.Windows.Forms.Button();
+            this.button_Cancel = new System.Windows.Forms.Button();
+            this.textBox_author2 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox_title2 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 61);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 21);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ID:";
+            // 
+            // textBox_id
+            // 
+            this.textBox_id.Location = new System.Drawing.Point(143, 58);
+            this.textBox_id.Name = "textBox_id";
+            this.textBox_id.Size = new System.Drawing.Size(264, 31);
+            this.textBox_id.TabIndex = 1;
+            // 
+            // textBox_title
+            // 
+            this.textBox_title.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox_title.Location = new System.Drawing.Point(143, 95);
+            this.textBox_title.Name = "textBox_title";
+            this.textBox_title.Size = new System.Drawing.Size(652, 31);
+            this.textBox_title.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 98);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 21);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Title:";
+            // 
+            // textBox_author
+            // 
+            this.textBox_author.Location = new System.Drawing.Point(143, 193);
+            this.textBox_author.Name = "textBox_author";
+            this.textBox_author.Size = new System.Drawing.Size(264, 31);
+            this.textBox_author.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 196);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(87, 21);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Author:";
+            // 
+            // button_OK
+            // 
+            this.button_OK.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button_OK.Location = new System.Drawing.Point(489, 438);
+            this.button_OK.Name = "button_OK";
+            this.button_OK.Size = new System.Drawing.Size(150, 45);
+            this.button_OK.TabIndex = 6;
+            this.button_OK.Text = "OK";
+            this.button_OK.UseVisualStyleBackColor = true;
+            this.button_OK.Click += new System.EventHandler(this.button_OK_Click);
+            // 
+            // button_Cancel
+            // 
+            this.button_Cancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button_Cancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.button_Cancel.Location = new System.Drawing.Point(645, 438);
+            this.button_Cancel.Name = "button_Cancel";
+            this.button_Cancel.Size = new System.Drawing.Size(150, 45);
+            this.button_Cancel.TabIndex = 7;
+            this.button_Cancel.Text = "Cancel";
+            this.button_Cancel.UseVisualStyleBackColor = true;
+            this.button_Cancel.Click += new System.EventHandler(this.button_Cancel_Click);
+            // 
+            // textBox_author2
+            // 
+            this.textBox_author2.Location = new System.Drawing.Point(143, 230);
+            this.textBox_author2.Name = "textBox_author2";
+            this.textBox_author2.Size = new System.Drawing.Size(264, 31);
+            this.textBox_author2.TabIndex = 9;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 233);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(87, 21);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Author:";
+            // 
+            // textBox_title2
+            // 
+            this.textBox_title2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox_title2.Location = new System.Drawing.Point(143, 132);
+            this.textBox_title2.Name = "textBox_title2";
+            this.textBox_title2.Size = new System.Drawing.Size(652, 31);
+            this.textBox_title2.TabIndex = 11;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(13, 135);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(76, 21);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Title:";
+            // 
+            // DocumentDialog
+            // 
+            this.AcceptButton = this.button_OK;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.button_Cancel;
+            this.ClientSize = new System.Drawing.Size(807, 495);
+            this.Controls.Add(this.textBox_title2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox_author2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button_Cancel);
+            this.Controls.Add(this.button_OK);
+            this.Controls.Add(this.textBox_author);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox_title);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox_id);
+            this.Controls.Add(this.label1);
+            this.Name = "DocumentDialog";
+            this.Text = "DocumentDialog";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox_id;
+        private System.Windows.Forms.TextBox textBox_title;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox_author;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button_OK;
+        private System.Windows.Forms.Button button_Cancel;
+        private System.Windows.Forms.TextBox textBox_author2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox_title2;
+        private System.Windows.Forms.Label label5;
+    }
+}
