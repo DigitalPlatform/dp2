0a455b67b2e7f35bd4c79458de0ad3128c500418
谢涛 <xietao@dp2003.com>
2021-10-26 23:25:28 +0800

    整理代码

--- TestDp2Library/TestReadersMonitor.cs
+++ TestDp2Library/TestReadersMonitor.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 using Xunit;
 
 using DigitalPlatform.LibraryServer;
+using DigitalPlatform.Text;
 
 namespace TestDp2Library
 {
@@ -15,13 +16,6 @@ namespace TestDp2Library
     /// </summary>
     public class TestReadersMonitor
     {
-        /*
-Tue, 26 Oct 2021 16:13:50 +0800
-5day
--5day,-2day,-1day
-        * 
-         * 
-         * */
         [Theory]
         // *** 整数形态
         // 已经过了 10-31, 属于超期的情况了
@@ -67,7 +61,7 @@ Tue, 26 Oct 2021 16:13:50 +0800
         [InlineData("2021-10-25", "Tue, 26 Oct 2021 16:13:50 +0800",
             "5day", "50%", 0, new int[] { })]
 
-        public void TestReadersMonitor_CheckNotifyPoint(
+        public void TestReadersMonitor_CheckNotifyPoint_1(
             string today,
             string borrowDate,
             string period,
@@ -79,8 +73,9 @@ Tue, 26 Oct 2021 16:13:50 +0800
             {
                 for (int i = 0; i < expect_indices.Length; i++)
                 {
-                    // 索引值必须小于数组尺寸
-                    Assert.True(expect_indices[i] >= 0 && expect_indices[i] < expect_indices.Length);
+                    // 索引值必须小于检查点个数
+                    var checkpoint_count = StringUtil.SplitList(notifyDef).Count;
+                    Assert.True(expect_indices[i] >= 0 && expect_indices[i] < checkpoint_count);
                 }
             }
 
@@ -109,5 +104,59 @@ Tue, 26 Oct 2021 16:13:50 +0800
                 }
             }
         }
+
+        [Theory]
+        // 有检查点在范围之外
+        // -7day 这一时刻在 5 天范围之外，不会被返回 
+        [InlineData("2021-10-26", "Tue, 26 Oct 2021 16:13:50 +0800",
+            "5day", "-7day,-5day,-2day,-1day", 0, new int[] { 1 })]
+        [InlineData("2021-10-31", "Tue, 26 Oct 2021 16:13:50 +0800",
+            "5day", "-5day,6day", 0, new int[] { 0 })]
+
+        public void TestReadersMonitor_CheckNotifyPoint_2(
+    string today,
+    string borrowDate,
+    string period,
+    string notifyDef,
+    int expected_ret,
+    int[] expect_indices)
+        {
+            if (expect_indices != null)
+            {
+                for (int i = 0; i < expect_indices.Length; i++)
+                {
+                    // 索引值必须小于检查点个数
+                    var checkpoint_count = StringUtil.SplitList(notifyDef).Count;
+                    Assert.True(expect_indices[i] >= 0 && expect_indices[i] < checkpoint_count);
+                }
+            }
+
+            DateTime today_time = DateTime.Parse(today);
+
+            // 检查每个通知点，返回当前时间已经达到或者超过了通知点的那些检查点的下标
+            // return:
+            //      -1  数据格式错误
+            //      0   成功
+            int nRet = LibraryApplication.CheckNotifyPoint(
+                null,
+                null,   // Calendar calendar,
+                today_time.ToUniversalTime(),
+                borrowDate,
+                period,
+                notifyDef,
+                out List<int> indices,
+                out string strError);
+            Assert.Equal(expected_ret, nRet);
+            if (expect_indices != null)
+            {
+                Assert.Equal(expect_indices.Length, indices.Count);
+                for (int i = 0; i < expect_indices.Length; i++)
+                {
+                    Assert.Equal(expect_indices[i], indices[i]);
+                }
+            }
+        }
+
+
     }
 }
