fcda1a6b75dea5a3d54e71dc9249fe59b1adb402
removedporn <86824510+removedporn@users.noreply.github.com>
2022-03-18 08:33:36 +0800

    Delete dp2SSL directory

--- dp2SSL/buildgreen.bat
+++
@@ -1,4 +0,0 @@
-
-..\greenutility\bin\debug\greenutility -action:build -source:c:\dp2\dp2ssl -target:c:\publish\dp2ssl\v1
-
-xcopy ..\greensetup\bin\release\netcoreapp3.1\publish\greensetup.exe c:\publish\dp2ssl\v1 /Y
