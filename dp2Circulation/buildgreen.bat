c627cd8d354aafe0f57ba596897fa8374622880b
removedporn <86824510+removedporn@users.noreply.github.com>
2022-03-18 08:24:35 +0800

    Delete dp2Circulation directory

--- dp2Circulation/buildgreen.bat
+++
@@ -1,2 +0,0 @@
-
-..\greenutility\bin\debug\greenutility -action:build -source:c:\dp2\dp2circulation -target:c:\publish\dp2circulation\v3
--- dp2Circulation/copysqlite.bat
+++
@@ -1,6 +0,0 @@
-
-md x86
-xcopy .\bin\debug\x86\sqlite.interop.dll x86 /Y
-
-md x64
-xcopy .\bin\debug\x64\sqlite.interop.dll x64 /Y
